8b80b1b0d47aaf74ebf044cad088c4d724e2dab8
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2023-06-08 14:33:15 -0500

    mute then unmute

--- src/Dolly.js
+++ src/Dolly.js
@@ -5,7 +5,7 @@ const Dolly = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    document.getElementById("ninetofive").src = "https://www.youtube.com/embed/Lq1JD-bcGTo?controls=0&autoplay=1&mute=1";
+    document.getElementById("ninetofive").src = "https://www.youtube.com/embed/Lq1JD-bcGTo?controls=0&autoplay=1&mute=0";
 
     setTimeout(() => {
       navigate("/congratulations")
@@ -13,7 +13,7 @@ const Dolly = () => {
   })
   return (
     <div className="App" style={{flexDirection: "column"}}>
-      <iframe id="ninetofive" width="250vw" height="auto" src="https://www.youtube.com/embed/Lq1JD-bcGTo?controls=0" title="Dolly Parton - 9 To 5 (Lyrics)" frameBorder="0" allow="autoplay playsinline" allowFullScreen></iframe>
+      <iframe id="ninetofive" width="250vw" height="auto" src="https://www.youtube.com/embed/Lq1JD-bcGTo?controls=0&mute=1" title="Dolly Parton - 9 To 5 (Lyrics)" frameBorder="0" allow="autoplay playsinline" allowFullScreen></iframe>
       <div id="hack-div" onClick={() => navigate("/i_still_love_you")}></div>
       <h1>{`Choose how you will suffer for the next 3 minutes :)`}</h1>
     </div>
