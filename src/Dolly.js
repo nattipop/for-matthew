af2c0c16bc5d00afee582b546e541c523988dca3
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2023-06-08 14:30:02 -0500

    mute

--- src/Dolly.js
+++ src/Dolly.js
@@ -5,7 +5,7 @@ const Dolly = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    document.getElementById("ninetofive").src = "https://www.youtube.com/embed/Lq1JD-bcGTo?controls=0&autoplay=1";
+    document.getElementById("ninetofive").src = "https://www.youtube.com/embed/Lq1JD-bcGTo?controls=0&autoplay=1&mute=1";
 
     setTimeout(() => {
       navigate("/congratulations")
@@ -15,7 +15,7 @@ const Dolly = () => {
     <div className="App" style={{flexDirection: "column"}}>
       <iframe id="ninetofive" width="250vw" height="auto" src="https://www.youtube.com/embed/Lq1JD-bcGTo?controls=0" title="Dolly Parton - 9 To 5 (Lyrics)" frameBorder="0" allow="autoplay playsinline" allowFullScreen></iframe>
       <div id="hack-div" onClick={() => navigate("/i_still_love_you")}></div>
-      <h1>{`Choose which one you will suffer through for 3 minutes :)`}</h1>
+      <h1>{`Choose how you will suffer for the next 3 minutes :)`}</h1>
     </div>
   )
 }
--- src/Tim.js
+++ src/Tim.js
@@ -5,7 +5,7 @@ const Tim = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    document.getElementById("jtsys").src = "https://www.youtube.com/embed/EMlhAds4PWg?controls=0&autoplay=1";
+    document.getElementById("jtsys").src = "https://www.youtube.com/embed/EMlhAds4PWg?controls=0&autoplay=1&mute=1";
 
     setTimeout(() => {
       navigate("/congratulations")
@@ -15,7 +15,7 @@ const Tim = () => {
     <div className="App" style={{flexDirection: "column"}}>
       <iframe id="jtsys" width="250vw" height="auto" src="https://www.youtube.com/embed/EMlhAds4PWg" title="Just To See You Smile" frameBorder="0" allow="autoplay playsinline" allowFullScreen></iframe>
       <div id="hack-div" onClick={() => navigate("/i_apologize")}></div>
-      <h1>{`Choose which one you will suffer through for 3 minutes :)`}</h1>
+      <h1>{`Choose how you will suffer for the next 3 minutes :)`}</h1>
     </div>
   )
 }
